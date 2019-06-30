0ed3279527780d85738eb807c9c4b031399ef42f
Máté Kis-Király <mate.kis-kiraly@nemesys.fi>
2019-06-30 09:40:57 +0300

    Init

---
+++ App.tsx
@@ -0,0 +1,39 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, { Component } from "react";
+import { Platform, StyleSheet, Text, View } from "react-native";
+import MapView from "react-native-maps"; // remove PROVIDER_GOOGLE import if not using Google Maps
+
+const styles = StyleSheet.create({
+  container: {
+    ...StyleSheet.absoluteFillObject,
+    height: 400,
+    width: 400,
+    justifyContent: "flex-end",
+    alignItems: "center"
+  },
+  map: {
+    ...StyleSheet.absoluteFillObject
+  }
+});
+
+export default () => (
+  <View style={styles.container}>
+    <MapView
+      style={styles.map}
+      region={{
+        latitude: 37.78825,
+        longitude: -122.4324,
+        latitudeDelta: 0.015,
+        longitudeDelta: 0.0121
+      }}
+    />
+    <Text>AAAAAAA</Text>
+  </View>
+);
